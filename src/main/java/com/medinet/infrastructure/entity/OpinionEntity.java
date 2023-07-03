b64fea3a3c97db8f72720151f525c7d8aec748e9
Artur <artur.augustyn26@gmail.com>
2023-07-03 15:29:52 +0200

    first idea of opinion object

---
+++ src/main/java/com/medinet/infrastructure/entity/OpinionEntity.java
@@ -0,0 +1,38 @@
+package com.medinet.infrastructure.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"opinionId"})
+@Entity
+@Table(name = "opinion")
+public class OpinionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "opinion_id")
+    private Integer opinionId;
+
+    @Column(name = "date")
+    private OffsetDateTime dateOfCreateOpion;
+    @Column(name = "note")
+    private String note;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "patient_id")
+    private PatientEntity patient;
+
+    @JsonBackReference
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "doctor_id")
+    private DoctorEntity doctor;
+
+}
