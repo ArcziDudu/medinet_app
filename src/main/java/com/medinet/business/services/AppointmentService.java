cb77ff4fc7170a641967a58b8071838003b94597
Artur <artur.augustyn26@gmail.com>
2023-07-04 14:16:10 +0200

    added first part of appointment mechanism

--- src/main/java/com/medinet/business/services/AppointmentService.java
+++ src/main/java/com/medinet/business/services/AppointmentService.java
@@ -5,9 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
+import java.util.Random;
+import java.util.UUID;
+
 @Service
 @AllArgsConstructor
 @Slf4j
 public class AppointmentService {
     private final AppointmentDao appointmentDao;
+
+    public OffsetDateTime getIssueDate() {
+        return OffsetDateTime.now();
+    }
+
+    public String getVisitNumber() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
+    }
 }
