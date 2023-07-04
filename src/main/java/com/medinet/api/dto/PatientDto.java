24cd927437111fc6b5d4f62b894e27df5d32c056
Artur <artur.augustyn26@gmail.com>
2023-07-04 12:22:00 +0200

    added opinion rendering

--- src/main/java/com/medinet/api/dto/PatientDto.java
+++ src/main/java/com/medinet/api/dto/PatientDto.java
@@ -1,10 +1,13 @@
 package com.medinet.api.dto;
 
 import com.medinet.infrastructure.entity.AddressEntity;
+import com.medinet.infrastructure.entity.OpinionEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Data
 @Builder
@@ -17,4 +20,5 @@ public class PatientDto {
     private String surname;
     private String phoneNumber;
     private AddressEntity address;
+    private Set<OpinionEntity> opinions;
 }
