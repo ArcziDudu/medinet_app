8259551a0e4c62336c936f36f7e85972181d1770
Artur <artur.augustyn26@gmail.com>
2023-07-04 16:38:02 +0200

    added mechanism - Remove the doctor's free hour if someone is calling it off

--- src/main/java/com/medinet/business/services/AppointmentService.java
+++ src/main/java/com/medinet/business/services/AppointmentService.java
@@ -1,7 +1,9 @@
 package com.medinet.business.services;
 
+import com.medinet.api.dto.DoctorDto;
 import com.medinet.business.dao.AppointmentDao;
 import com.medinet.infrastructure.entity.AppointmentEntity;
+import com.medinet.infrastructure.entity.CalendarEntity;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,31 +12,35 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class AppointmentService {
     private final AppointmentDao appointmentDao;
-
-    public OffsetDateTime getIssueDate() {
-        return OffsetDateTime.now();
-    }
+    private final DoctorService doctorService;
 
     public String getVisitNumber() {
         UUID uuid = UUID.randomUUID();
         return uuid.toString();
     }
 
-    public OffsetDateTime convertStringToOffsetDateTime(String selectedHour) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        return OffsetDateTime.parse(selectedHour, formatter);
-    }
 
     @Transactional
     public void processAppointment(AppointmentEntity appointment) {
+
+
+        doctorService.findDoctorById(appointment.getDoctor().getDoctorId())
+                .getCalendars()
+                .forEach(calendar -> calendar.getHours()
+                        .removeIf(value -> value.equals(appointment.getTimeOfVisit())));
+
         appointmentDao.saveAppointment(appointment);
     }
 
