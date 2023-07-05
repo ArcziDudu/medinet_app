3d63af60fc252ee1b6796f40309c878b3401d4a1
Artur <artur.augustyn26@gmail.com>
2023-07-05 15:31:23 +0200

    added rendering mechanism for pending appointments

--- src/main/java/com/medinet/infrastructure/repository/AppointmentRepository.java
+++ src/main/java/com/medinet/infrastructure/repository/AppointmentRepository.java
@@ -1,5 +1,6 @@
 package com.medinet.infrastructure.repository;
 
+import com.medinet.api.dto.AppointmentDto;
 import com.medinet.business.dao.AppointmentDao;
 import com.medinet.infrastructure.entity.AppointmentEntity;
 import com.medinet.infrastructure.repository.jpa.AppointmentJpaRepository;
@@ -7,10 +8,14 @@ import com.medinet.infrastructure.repository.mapper.AppointmentMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Repository
 @AllArgsConstructor
 public class AppointmentRepository implements AppointmentDao {
     private final AppointmentJpaRepository appointmentJpaRepository;
+    private final AppointmentMapper appointmentMapper;
     @Override
     public void saveAppointment(AppointmentEntity appointment) {
         appointmentJpaRepository.save(appointment);
@@ -19,5 +24,11 @@ public class AppointmentRepository implements AppointmentDao {
     @Override
     public void removeAppointment(Integer appointmentID) {
         appointmentJpaRepository.deleteByAppointmentId(appointmentID);
+    }
+
+    @Override
+    public List<AppointmentDto> findAllByStatus(String status) {
+        return appointmentJpaRepository.findAllByStatus(status).stream()
+                .map(appointmentMapper::mapFromEntity).collect(Collectors.toList());
     }
 }
