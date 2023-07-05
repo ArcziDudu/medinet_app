f0fb216302c9946ee71c05bf7e47226b0923b82e
Artur <artur.augustyn26@gmail.com>
2023-07-05 10:18:37 +0200

    repaired bootstrap mechanism

--- src/main/resources/db/migration/V2_2__init_medinet_init_doctors.sql
+++ src/main/resources/db/migration/V2_2__init_medinet_init_doctors.sql
@@ -1,2 +1,3 @@
 insert into doctor (doctor_id, name, surname, email, price_for_visit, specialization, address_id) values (1, 'Ralf', 'D''Ambrosio', 'rdambrosio0@dmoz.org', 144.58, 'Seksuolog', 1);
+insert into doctor (doctor_id, name, surname, email, price_for_visit, specialization, address_id) values (2, 'Artur', 'Augustyn', 'artur0@dmoz.org', 144.58, 'Kardiolog', 2);
 
